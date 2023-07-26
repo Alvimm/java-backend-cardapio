34d3356303975328fafe93583fdd4ef8c6348934
Filipe Alvim <filipe2012alvim@gmail.com>
2023-07-26 12:20:58 -0300

    Add the entity of Food

---
+++ src/main/java/com/alvimm/cardapio/entities/Food.java
@@ -0,0 +1,72 @@
+package com.alvimm.cardapio.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Table(name = "foods")
+@Entity(name = "foods")
+public class Food {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String image;
+    private Integer price;
+
+    public Food() {
+    }
+
+    public Food(FoodRequestDTO data) {
+        this.title = data.title();
+        this.image = data.image();
+        this.price = data.price();
+    }
+
+    public Food(Long id, String title, String image, Integer price) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Food food = (Food) o;
+        return Objects.equals(id, food.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
